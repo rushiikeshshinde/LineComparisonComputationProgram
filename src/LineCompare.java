e25da9915f597d72579c56dbe386cbcc7e6d665a
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-16 12:57:13 +0530

    Added: Welcome message is added

---
+++ src/LineCompare.java
@@ -0,0 +1,11 @@
+public class LineCompare {
+
+    // Main Method
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+
+}
